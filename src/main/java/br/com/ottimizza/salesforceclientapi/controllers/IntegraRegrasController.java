b83fd2fb01de295e749079a4639f731c0a7f1a15
Daniel Steil <dani.steil@hotmail.com>
2021-09-03 10:32:41 -0300

    criando novo controller e alterando url liberada

---
+++ src/main/java/br/com/ottimizza/salesforceclientapi/controllers/IntegraRegrasController.java
@@ -0,0 +1,28 @@
+package br.com.ottimizza.salesforceclientapi.controllers;
+
+import javax.inject.Inject;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import br.com.ottimizza.salesforceclientapi.services.SalesforceService;
+
+@RestController
+@RequestMapping("integra/v1/salesforce")
+public class IntegraRegrasController {
+
+    @Inject
+    SalesforceService salesforceService;
+    
+    @PostMapping("/{chave}/composite/tree/{objectId}")
+    public ResponseEntity<?> createMultipleRecordsListener(@PathVariable("chave") String chave,
+                                    @PathVariable("objectId") String objectId, 
+                                    @RequestBody String object) throws Exception {
+        return ResponseEntity.ok(salesforceService.insertMultipleListener(chave, objectId, object));
+    }
+
+}
